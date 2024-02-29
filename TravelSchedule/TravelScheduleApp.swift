acc6a22e7c6ae4a304aa8b47b693f10a9ab51d53
Anton Kashnikov <anton.kashnikoff@icloud.com>
2024-02-29 15:38:31 +0100

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Антон Кашников on 29/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
