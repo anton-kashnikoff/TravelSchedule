6f51735459eb9927fb667452f0fc9358b4eabeae
Anton Kashnikov <anton.kashnikoff@icloud.com>
2024-04-15 18:11:18 +0200

    feat(UI): add Splash Screen

---
+++ TravelSchedule/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  TravelSchedule
+//
+//  Created by Антон Кашников on 15/04/2024.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
---
+++ TravelSchedule/ViewCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  ViewCoordinator.swift
+//  TravelSchedule
+//
+//  Created by Антон Кашников on 15/04/2024.
+//
+
+import SwiftUI
+
+struct ViewCoordinator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewCoordinator()
+}
